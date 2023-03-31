0af49ff87604202fc6ce31593d21b5b61463fbb3
Quentin Michalet <qmichalet@mini-de-quentin.home>
2023-03-31 18:20:49 +0200

    new friends list widget component

---
+++ src/scenes/widgets/FriendsListWidget.jsx
@@ -0,0 +1,57 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const FriendsListWidget = ({ userId }) => {
+	const dispatch = useDispatch();
+	const token = useSelector((state) => state.token);
+	const friends = useSelector((state) => state.user.friends);
+	const { palette } = useTheme();
+	const { dark } = palette.neutral;
+
+	const getFriends = async () => {
+		const response = await fetch(
+			`http://localhost:3001/users/${userId}/friends`,
+			{
+				method: "GET",
+				headers: { Authorization: `Bearer ${token}` }
+			}
+		);
+
+		const data = await response.json();
+		dispatch(setFriends({ friends: data }));
+	};
+
+	useEffect(() => {
+		getFriends();
+	}, []);
+
+	return (
+		<WidgetWrapper>
+			<Typography
+				color={dark}
+				variant="h5"
+				fontWeight="500"
+				sx={{ mb: "1.5rem" }}
+			>
+				Friends List
+			</Typography>
+			<Box display="flex" flexDirection="column" gap="1.5rem">
+				{friends.map((friend) => (
+					<Friend
+						key={friend._id}
+						friendId={friend._id}
+						name={`${friend.firstName} ${friend.lastName}`}
+						subtitle={friend.occupation}
+						userPicturePath={friend.picturePath}
+					/>
+				))}
+			</Box>
+		</WidgetWrapper>
+	);
+};
+
+export default FriendsListWidget;
